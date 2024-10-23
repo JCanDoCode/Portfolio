2d145ee32b96979e755a02285ec8bfe6055998e3
JCanDoCode <156962029+JCanDoCode@users.noreply.github.com>
2024-10-22 22:46:44 -0700

    Update 13.1
    Fixed Footer Copyright

--- components/footer.php
+++ components/footer.php
@@ -1,16 +1,16 @@
 <footer>
-    <div class="footer-wordmark">
-        <a href="./index.php">
-            <svg class="wordmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 600">
-                <text class="cls-1" transform="translate(352.25 345.8)"><tspan x="0" y="0">CanArt</tspan></text>
-                <path class="cls-2" d="M211.16,481.81c-244.85,36.27,58.33-155.89,23.38-288.92-39.61-3.51-69.54-19.56-69.54-38.83,0-21.84,38.44-20.88,85.86-20.88s85.86-.95,85.86,20.88c0,15.4-19.11,28.74-47.01,35.27,19.74,32.3,149.24,258.73-78.55,292.48Z"/>
-            </svg>
-        </a>
+    <div class="footer-container">
+        <div class="footer-wordmark">
+            <a href="./index.php">
+                <svg class="wordmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 600">
+                    <text class="cls-1" transform="translate(352.25 345.8)"><tspan x="0" y="0">CanArt</tspan></text>
+                    <path class="cls-2" d="M211.16,481.81c-244.85,36.27,58.33-155.89,23.38-288.92-39.61-3.51-69.54-19.56-69.54-38.83,0-21.84,38.44-20.88,85.86-20.88s85.86-.95,85.86,20.88c0,15.4-19.11,28.74-47.01,35.27,19.74,32.3,149.24,258.73-78.55,292.48Z"/>
+                </svg>
+            </a>
+        </div>
+        <div class="linkedin-icon">
+            <a href="https://www.linkedin.com/in/josel-canlas-1441b1291/" target="_blank"><img src="./media/img/linkedin-icon.png" alt="LinkedIn"></a>
+        </div>
     </div>
-    <div class="footer-text">
-        <p>&copy; JCanArt(Josel Canlas) 2024</p>
-    </div>
-    <div class="linkedin-icon">
-        <a href="https://www.linkedin.com/in/josel-canlas-1441b1291/" target="_blank"><img src="./media/img/linkedin-icon.png" alt="LinkedIn"></a>
-    </div>
+    <p class="copyright">&copy; JCanArt(Josel Canlas) 2024</p>
 </footer>
